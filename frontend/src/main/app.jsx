766818cc7bdb77d99aa4c5c05f35fe4a0809e7e0
Vinicius Martins <vinibitmartins@gmail.com>
2024-12-03 21:34:45 -0300

    Initialize frontend with React setup and basic structure

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,14 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>App</h1>
+        </div>
+    )    
+}
+
+export default App
